a45b884a77ff3fe9e5326f90fe4af31fd1c71f0b
Bpazy <hanziyuan08@outlook.com>
2016-04-21 22:25:01 +0800

    Move Zeroes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] old = {1, 2, 9, 9};
-        int[] oldN = s.plusOne(old);
-        System.out.println(Arrays.toString(oldN));
+        int[] old = {0, 1, 2, 3, 0, 1, 0, 9};
+        System.out.println(Arrays.toString(old));
+        s.moveZeroes(old);
+        System.out.println(Arrays.toString(old));
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -4,6 +4,17 @@ import java.util.*;
 
 public class Solution {
 
+    public void moveZeroes(int[] nums) {
+        for (int i = nums.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] == 0) {
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = 0;
+                }
+            }
+        }
+    }
+
     public int countPrimes(int n) {
         if (n <= 2) return 0;
         boolean[] flag = new boolean[n];

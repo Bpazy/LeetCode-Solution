4e1fa7f3d658ac76d0f9d73f949e3d0a414c3567
Bpazy <hanziyuan08@outlook.com>
2016-04-22 15:40:38 +0800

    Contains Duplicate

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] old = {0, 1, 2, 3, 0, 1, 0, 9};
+        int[] old = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(old));
-        s.moveZeroes(old);
-        System.out.println(Arrays.toString(old));
+        System.out.println(s.containsDuplicate(old));
     }
 }
--- src/com/company/Solution.java
+++ src/com/company/Solution.java
@@ -4,6 +4,15 @@ import java.util.*;
 
 public class Solution {
 
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.contains(num)) set.add(num);
+            else return true;
+        }
+        return false;
+    }
+
     public void moveZeroes(int[] nums) {
         for (int i = nums.length - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {

2334ddf6e2a26e67bd3a12f28d37287b7b4a785e
Bpazy <hanziyuan08@outlook.com>
2016-04-22 22:01:52 +0800

    use JUnit4

---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,76 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    @Test
+    public void testContainsNearbyDuplicate() throws Exception {
+
+    }
+
+    @Test
+    public void testContainsDuplicate() throws Exception {
+
+    }
+
+    @Test
+    public void testMoveZeroes() throws Exception {
+
+    }
+
+    @Test
+    public void testCountPrimes() throws Exception {
+
+    }
+
+    @Test
+    public void testPlusOne() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveElement() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveDuplicates() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveDuplicates2() throws Exception {
+
+    }
+
+    @Test
+    public void testHasCycle() throws Exception {
+
+    }
+
+    @Test
+    public void testSortList() throws Exception {
+
+    }
+
+    @Test
+    public void testMergeTwoLists() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteNode() throws Exception {
+
+    }
+
+    @Test
+    public void testRemoveNthFromEnd() throws Exception {
+
+    }
+
+    @Test
+    public void testSwapPairs() throws Exception {
+
+    }
+}
